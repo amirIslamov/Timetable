40ca2f5c54d41df9aa87b5b51c18a37ba074082b
amir <smetanagosha@gmail.com>
2021-12-19 20:20:44 +0200

    Abstract pattern created

---
+++ FilteringOrderingPagination/Models/Pattern.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using FilteringOrderingPagination.Models.Specifications;
+
+namespace FilteringOrderingPagination.Models;
+
+public abstract class Pattern<T>
+{
+    protected abstract IList<Expression<Func<T, bool>>> GetPredicateList();
+
+    public Specification<TEntity> AppliedTo<TEntity>(Expression<Func<TEntity, T>> selector)
+    {
+        var exprList = GetPredicateList();
+        if (exprList == null || exprList.Count == 0)
+            return new Specification<TEntity>(x => true);
+
+        var resultExpression = exprList
+            .Select(e => selector.Chain(e))
+            .Aggregate((l, r) => l.And(r));
+
+        return new Specification<TEntity>(resultExpression);
+    }
+}
